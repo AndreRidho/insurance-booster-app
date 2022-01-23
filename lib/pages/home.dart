d88fb22b8509e107fd541955c0fb92a017e4aea9
Andre Ridho <mbridho@gmail.com>
2022-01-23 17:39:16 +0800

    Homepage

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,53 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Insurance Booster App'),
       ),
+      body: Column(
+        children: [
+          GestureDetector(
+            onTap: () {},
+            child: const OptionCard(
+                asset: 'assets/images/up-arrow.png',
+                text: 'Upload insurance document'),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: const OptionCard(
+                asset: 'assets/images/gift.png',
+                text: 'View available packages'),
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: const OptionCard(
+                asset: 'assets/images/exchange.png', text: 'Refer a friend'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OptionCard extends StatelessWidget {
+  const OptionCard({Key? key, this.asset, this.text}) : super(key: key);
+  final asset;
+  final text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          children: <Widget>[
+            Image(
+              image: AssetImage(asset),
+              height: 30,
+              width: 30,
+            ),
+            const SizedBox(width: 30),
+            Text(text)
+          ],
+        ),
+      ),
     );
   }
 }
